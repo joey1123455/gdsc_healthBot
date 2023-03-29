50bb988a9c30b32c3401aecb0790999a1ad0eb80
olamideakintayo <divineolamide977@gmail.com>
2023-03-29 04:49:43 +0100

    location api

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
- // Importing necessary dependencies and styles
+// Importing necessary dependencies and styles
 import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
-import { useState, useEffect } from "react";
-import Swal from 'sweetalert2'
-import Modal from "react-modal";
-import './react-modal.css'
-import axios from 'axios';
+import { useState, useEffect, useCallback } from "react";
+import Swal from "sweetalert2";
+import ReactModal from "react-modal";
+import "./index.css";
+import axios from "axios";
 import {
   MainContainer,
   ChatContainer,
@@ -15,15 +15,38 @@ import {
 } from "@chatscope/chat-ui-kit-react";
 
 // Setting the app element to be the root element of your app for accessibility purposes
-Modal.setAppElement("#root");
+ReactModal.setAppElement("#root");
 
 function App() {
   // Define state variables
   const [messages, setMessages] = useState([]); // store chat messages
   const [typing, setTyping] = useState(false); // show typing indicator
-  const [location, setLocation] = useState(null); // store user's location
-  const [returnedData, setReturnedData] = useState(null); // store data returned from the backend
-  const [modalIsOpen, setModalIsOpen] = useState(false); // show/hide modal window
+  const [userLocation, setUserLocation] = useState({
+    latitude: null,
+    longitude: null,
+  });
+  const [responseData, setResponseData] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleBeforeUnload = useCallback((event) => {
+    event.preventDefault();
+    Swal.fire({
+      title:
+        "We respect your privacy, if you refresh this page all chat history will be cleared. Refresh?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        clearChats();
+        window.removeEventListener("beforeunload", handleBeforeUnload);
+        Swal.fire("Deleted!", "Your Chats has been deleted.", "success");
+      }
+    });
+  });
 
   // Add an event listener for the beforeunload event and remove it when the component unmounts
   useEffect(() => {
@@ -31,123 +54,97 @@ function App() {
     return () => {
       window.removeEventListener("beforeunload", handleBeforeUnload);
     };
-  }, []);
+  }, [handleBeforeUnload]);
 
   // Prompt user to confirm if they want to clear chat history when the page is being refreshed or closed
   const clearChats = () => {
     setMessages([]);
-  }
-
-  const handleBeforeUnload = (event) => {
-    event.preventDefault();
-    Swal.fire({
-      title: 'We respect your privacy, if you refresh this page all chat history will be cleared. Refresh?',
-      text: "You won't be able to revert this!",
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Yes, delete it!'
-    }).then((result) => {
-      if (result.isConfirmed) {
-        clearChats();
-        window.removeEventListener("beforeunload", handleBeforeUnload);
-        Swal.fire(
-          'Deleted!',
-          'Your Chats has been deleted.',
-          'success'
-        )
-      }
-    })
-  }
+  };
 
   // Send message to backend and add response to chat history
-  const apiUrl = "http://34.136.104.12:/api/"
+  const apiUrl = "http://34.136.104.12/api/";
   const handleSubmit = async (messageText) => {
-    axios.post(apiUrl, { msg: messageText }, { mode: 'no-cors' })
-      .then(response => {
+    axios
+      .post(apiUrl, { msg: messageText }, { mode: "no-cors" })
+      .then((response) => {
         if (response.status === 200) {
           // Format the response message
-          let message = response.data.res
-          let time = response.data.time
-          let newMessage = `${message} <br>${time}</br>`
-          const options = { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit' };
-          let date = new Date().toLocaleString(options).replace(/[/]/g, '-');
-          let userMessage = `${messageText} <br>${date}</br>`
+          let message = response.data.res;
+          let time = response.data.time;
+          let newMessage = `${message} <br>${time}</br>`;
+          const options = {
+            year: "numeric",
+            month: "2-digit",
+            day: "2-digit",
+            hour: "2-digit",
+            minute: "2-digit",
+            second: "2-digit",
+          };
+          let date = new Date().toLocaleString(options).replace(/[/]/g, "-");
+          let userMessage = `${messageText} <br>${date}</br>`;
           // Add the message to chat history
-          setMessages([...messages,{
-            message: userMessage,
-            direction: "outgoing"
-          },
-          {
-            message: newMessage
-          }]);
+          setMessages([
+            ...messages,
+            {
+              message: userMessage,
+              direction: "outgoing",
+            },
+            {
+              message: newMessage,
+            },
+          ]);
           // Hide typing indicator
           setTyping(false);
           console.log(response.data);
         } else {
-          console.error('Server returned an error:', response.status);
-    }
-  })
-  .catch(error => {
-    console.error(error);
-  });
-      setTyping(true);
-    
- }
-// This function retrieves the user's location using the browser's Geolocation API
-const getLocation = () => {
-  navigator.geolocation.getCurrentPosition((position) => {
-  setLocation({
-  latitude: position.coords.latitude,
-  longitude: position.coords.longitude,
-  });
-  });
+          console.error("Server returned an error:", response.status);
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    setTyping(true);
   };
-  
-  // This function sends the user's location data to a backend API using the Axios library
-  const sendLocationToBackend = () => {
-  if (location) {
-  axios.post("http://34.136.104.12:/api/location", {
-  lat: location.latitude,
-  lon: location.longitude,
-  })
-  .then((response) => {
-  // This logs the returned data and sets it in state
-  console.log(response.data);
-  setReturnedData(response.data);
-  setModalIsOpen(true);
-  })
-  .catch((error) => {
-  console.log(error);
-  });
-  }
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setUserLocation({
+        lat: position.coords.latitude,
+        lon: position.coords.longitude,
+      });
+    });
+  }, []);
+
+  //send location to the backend
+
+  const submitLocation = (event) => {
+    event.preventDefault();
+    axios
+      .post(`${apiUrl}location`, {
+        lat: userLocation.lat,
+        lon: userLocation.lon,
+      })
+      .then((response) => {
+        setResponseData(response.data);
+        setIsModalOpen(true);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   };
-  
-  // This function closes the modal
-  const closeModal = () => {
-  setModalIsOpen(false);
+
+  const customModalStyles = {
+    content: {
+      color: "black",
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
+    },
   };
-  
-  // This component renders the content of the modal from the locationAPi
-  function ModalContent(props) {
-  const { desc, list } = props;
-  
-  return (
-  <div className="modal-content">
-  <h2>{desc}</h2>
-  {list && list.map((item, index) => (
-  <div key={index}>
-  <h3>{item.name}</h3>
-  <p>{item.vicinity}</p>
-  <p dangerouslySetInnerHTML={{ __html: item.googleMap_link }}></p>
-  <p>{item.open}</p>
-  </div>
-  ))}
-  </div>
-  );
-  }
-  
 
   return (
     <div className="">
@@ -155,42 +152,63 @@ const getLocation = () => {
         <p style={{ textAlign: "center" }}>The Cheer Bot😃</p>
       </div>
       <div>
-      <p style={{ textAlign: "center" }}>
-      Boost Your Spirit with the Cheer Bot Chatbot: Your Personal Cheerleader
+        <p style={{ textAlign: "center" }}>
+          Boost Your Spirit with the Cheer Bot Chatbot: Your Personal
+          Cheerleader
         </p>
-    </div>
-    <div>
-      <button onClick={getLocation}>Get Location</button>
-      <button onClick={sendLocationToBackend}>Send Location</button>
-      <Modal
-        isOpen={modalIsOpen}
-        onRequestClose={closeModal}
-        contentLabel="Returned Data Modal"
-        className="modal"
-      >
-       <ModalContent desc={returnedData && returnedData.desc} />
-        <button onClick={closeModal}>Close Modal</button>
-      </Modal>
-    </div>
-      <div className="wrapper">
-        <div className="box">
-          <MainContainer>
-            <ChatContainer>
-              <MessageList
-                scrollBehavior="smooth"
-                typingIndicator={
-                  typing ? (
-                    <TypingIndicator content="Cheer Bot😃" />
-                  ) : null
-                }
+      </div>
+      <div>
+        <div className="wrapper">
+          <div>
+            <form onSubmit={submitLocation}>
+              <button style={{ alignItems: "center"}} type="submit">Psychologists Around You</button>
+            </form>
+            <ReactModal isOpen={isModalOpen}>
+              <button
+                style={{ color: "black" }}
+                onClick={() => setIsModalOpen(false)}
               >
-                {messages.map((message, index) => (
-                  <Message key={index} model={message} />
-                ))}
-              </MessageList>
-              <MessageInput placeholder="Enter message" onSend={handleSubmit} />
-            </ChatContainer>
-          </MainContainer>
+                Close Modal
+              </button>
+              {isModalOpen && responseData && (
+                <div>
+                  <p style={{ color: "black", textAlign: "center" }}>Here are Psychologists around you:</p>
+                  <div style={{ color: "black" }}>
+                    {Object.keys(responseData.list).map((key) => {
+                      return (
+                        <div key={key}>
+                          <p>Hospital Name: {responseData.list[key].name}</p>
+                          <p>Address: {responseData.list[key].vicinity}</p>
+                          <p>Google Map Link: {responseData.list[key].googleMap_link}</p>
+                          <p>Availability: {responseData.list[key].open}</p>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
+              )}
+            </ReactModal>
+          </div>
+          <div className="box">
+            <MainContainer>
+              <ChatContainer>
+                <MessageList
+                  scrollBehavior="smooth"
+                  typingIndicator={
+                    typing ? <TypingIndicator content="Cheer Bot😃" /> : null
+                  }
+                >
+                  {messages.map((message, index) => (
+                    <Message key={index} model={message} />
+                  ))}
+                </MessageList>
+                <MessageInput
+                  placeholder="Enter message"
+                  onSend={handleSubmit}
+                />
+              </ChatContainer>
+            </MainContainer>
+          </div>
         </div>
       </div>
     </div>
